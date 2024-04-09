92673202fba54e653ca1c37e1ca7ddd20068dcd1
SyriusGit <henridoss.miranda@gmail.com>
2024-04-09 17:29:30 -0300

    novo commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+ainda é necessário: 
+corrigir o problema do checkbox do cadastro
+estilizar a página de matrículas
+implementar o react
+hospedar na web
--- script.js
+++ script.js
@@ -22,11 +22,7 @@ function login() {
     let userData = JSON.parse(localStorage.getItem(username));
 
     if (userData && password === userData.password) {
-        if (userData.isAdmin) {
-            window.location.href = '/admin.html';
-        } else {
             window.location.href = '/matricula.html';
-        }
     } else {
         alert('Usuário ou senha inválidos!');
     }
@@ -41,48 +37,3 @@ document.addEventListener('keydown', function(event) {
         }
     }
 });
-
-/***************Lógica das matrículas********************/
-let classes = [
-    { disciplina: 'Programação Front-End', professor: 'Prof. Elon', vagas: 10, dia: 'Segunda-feira' },
-    { disciplina: 'Programação Back-End', professor: 'Prof. Mark', vagas: 8, dia: 'Terça-feira' },
-    { disciplina: 'Testes de Software', professor: 'Prof. Jeff', vagas: 12, dia: 'Quarta-feira' },
-    { disciplina: 'Banco de Dados', professor: 'Prof. Jeff', vagas: 6, dia: 'Quinta-feira' },
-    { disciplina: 'Arquitetura da Informação', professor: 'Prof. Andrei', vagas: 15, dia: 'Sexta-feira' },
-    // Adicionar mais turmas
-];
-
-// Função para adicionar um card de disciplina
-function addClassCard(classData) {
-    let card = document.createElement('div');
-    card.className = 'card';
-    card.innerHTML = `
-        <h3>${classData.disciplina}</h3>
-        <p>Professor: ${classData.professor}</p>
-        <p>Vagas: ${classData.vagas}</p>
-        <p>Dia: ${classData.dia}</p>
-        <button onclick="matricular('${classData.disciplina}', '${classData.dia}')">Matricular-se</button>
-    `;
-    document.getElementById('classes').appendChild(card);
-}
-
-// Adiciona os cards de disciplinas
-classes.forEach(addClassCard);
-
-// Função para se matricular em uma disciplina
-function matricular(disciplina, dia) {
-    let dayColumn = document.getElementById(dia.toLowerCase()); // Convertendo para minúsculas para evitar problemas de capitalização
-    if (!dayColumn) {
-        console.error(`Coluna para ${dia} não encontrada.`);
-        return;
-    }
-    let tbody = dayColumn.parentElement.nextElementSibling; // Obtém o corpo da tabela
-    if (!tbody || tbody.tagName !== 'TBODY') {
-        console.error(`Corpo da tabela para ${dia} não encontrado ou incorreto.`);
-        return;
-    }
-
-    let row = document.createElement('tr');
-    row.innerHTML = `<td>${disciplina}</td>`;
-    tbody.appendChild(row);
-}
